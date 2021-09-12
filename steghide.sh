084f5b43160e907bc932555a311f42cb956d1c8b
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2021-09-12 22:18:02 +0530

    Update steghide.sh

--- steghide.sh
+++ steghide.sh
@@ -2,7 +2,6 @@ read -p $'\e[1;92m JPG-FILE: \e[0m' picture
 read -p $'\e[1;92m List with passwords: \e[0m' wl_pass 
 wl_pass="${wl_pass}"
 picture="${picture}"
-
 count_pass=$(wc -l $wl_pass | cut -d " " -f1)
 
 for fn in `cat $wl_pass`; do
